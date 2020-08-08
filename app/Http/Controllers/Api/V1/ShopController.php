7d7977981fefa986b91b908c2e4afccbacaeb951
zhuyongli <1778498309@qq.com>
2020-08-08 18:56:24 +0800

    111

--- app/Http/Controllers/Api/V1/ShopController.php
+++ app/Http/Controllers/Api/V1/ShopController.php
@@ -98,7 +98,7 @@ class ShopController extends BaseController
                     'appId' => $payment['appId'],
                     'timeStamp' => $payment['timestamp'],
                     'nonceStr' => $payment['nonceStr'],
-                    'package' => $payment['packageValue'],
+                    'package' => $payment['package'],
                     'signType' => $payment['signType'],
                 ];
                 if (!$payment){
