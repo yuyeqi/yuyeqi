eb6bee39e9f26bcb45be1fd8ac82ad7c5b7dfda5
zhuyongli <1778498309@qq.com>
2020-08-16 12:35:38 +0800

    11

--- app/Http/Service/UserService.php
+++ app/Http/Service/UserService.php
@@ -493,7 +493,7 @@ class UserService extends BaseSerivce
      */
     public function getExRecordLists($userInfo, $page, $limit)
     {
-        $field = ['id','deal_no', 'deal_score', 'goods_name', 'deal_status', 'date_format(create_time,"%Y-%m-%d")'];
+        $field = ['id','deal_no', 'deal_score', 'goods_name', 'deal_status', 'create_time as date_format(create_time,"%Y-%m-%d")'];
         $lists = ExchangeRecord::getExRecordLists($userInfo, $field, $page, $limit);
         return $this->getPageData($lists);
     }
