398f13a2ba6826ac159ce85898773333e7cb9827
zhuyongli <1778498309@qq.com>
2020-08-22 23:09:34 +0800

    11

--- resources/views/admin/goods/index.blade.php
+++ resources/views/admin/goods/index.blade.php
@@ -55,7 +55,13 @@
                 {type: 'checkbox',field: 'left',width: 30}
                 ,{field:'id', width:80, title: 'ID', sort: true}
                 ,{field:'goods_name', width:120, title: '商品名称'}
-                ,{field:'cate', title: '商品分类', width: 120}
+                ,{field:'cate', title: '商品分类', width: 120,templet:function (d) {
+                        if(d.cate != ''){
+                            return d.cate.cate_name;
+                        }else{
+                            return '';
+                        }
+                    }}
                 ,{field:'goods_cover',align: "center", title: '封面图',templet: function(d){
                         return  "<span id='pic_" + d.id + "'><img src='" + d.goods_cover + "'  style='width: 40px' lay-event='showPic' ></span>";
                     } }
