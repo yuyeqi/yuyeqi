6d85d884dbaf35237423a9a6e4be8f14ab20079f
zhuyongli <1778498309@qq.com>
2020-08-30 16:09:15 +0800

    11

--- app/Http/Controllers/Api/V1/ShopController.php
+++ app/Http/Controllers/Api/V1/ShopController.php
@@ -183,8 +183,8 @@ class ShopController extends BaseController
         $data = [
             'body' => '黄派门窗-订单支付',
             'out_trade_no' => $order->order_no,
-            //'total_fee' => $order->total_price,
-            'total_fee' => 1,
+            'total_fee' => $order->total_price,
+            //'total_fee' => 1,
             'trade_type' => 'JSAPI', // 请对应换成你的支付方式对应的值类型
             'openid' => $this->userInfo['openid'],
         ];
