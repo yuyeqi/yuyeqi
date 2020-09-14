c2892ecf2d2775e2910e6a1f1c295df2bd0be723
zhuyongli <1778498309@qq.com>
2020-09-14 23:49:15 +0800

    11

--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -222,7 +222,7 @@
                 var end_time = $("input[name='end']").val();
                 var deal_start_time = $("input[name='deal_start']").val();
                 var deal_end_time = $("input[name='deal_end']").val();
-                var status = $("input[name='status']").val();
+                var status = $("#selectStatus").val();
                 var user_name =  $("input[name='user_name']").val();
                 //执行重载
                 table.reload('tableId',{
